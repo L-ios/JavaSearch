a04993c46046b6283b6baa85487de06b8971bf01
GitHub Action <action@github.com>
2022-07-08 14:09:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/lios/javasearch/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lios/javasearch/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lios.javasearch.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lios.javasearch.MyBundle
 
 class MyApplicationService {
 
